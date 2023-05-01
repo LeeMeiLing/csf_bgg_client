a5fd90b1e40f8a3c1a28ba0d659bbc7439bc6407
LeeMeiLing <leem0060@outlook.com>
2023-05-01 23:43:54 +0800

    added mongo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription, map } from 'rxjs';
-import { Result } from './models';
+import { MongoResult, Result } from './models';
 
 const BGG_URL = 'http://localhost:8080/api/bgg'
+const BGG_MONGO_URL = 'http://localhost:8080/api/bgg/mongo' // for mongo
 
 @Component({
   selector: 'app-root',
@@ -17,8 +18,9 @@ export class AppComponent implements OnInit{
   form!:FormGroup
   result$!:Subscription
   notFound = false
-  results!: Result[] // method 1
+  // results!: Result[] // method 1
   // results: Result[] = [] // method 2
+  mongoResult!: MongoResult // method 3
 
   constructor(private fb:FormBuilder, private http:HttpClient){}
 
@@ -32,27 +34,27 @@ export class AppComponent implements OnInit{
     })
   }
 
-  // // ======== Method 1: ========
-  // // GET http://localhost:8080/api/bgg?name=NAME
-  search(){
+  // // // ======== Method 1: ========
+  // // // GET http://localhost:8080/api/bgg?name=NAME
+  // search(){
 
-    this.notFound = false // reset notFound
+  //   this.notFound = false // reset notFound
 
-    // create query param
-    const params = new HttpParams().set('name',this.form.value['name'])
+  //   // create query param
+  //   const params = new HttpParams().set('name',this.form.value['name'])
 
-    this.result$ = this.http.get(BGG_URL,{params}).subscribe({
-      next: r => {this.results = r as Result[]},
-      error: err => {
-        this.notFound = true
-        console.error('ERROR: ', err)
-      },
-      complete() {
-        console.warn('......COMPLETE: ')
-      }
-    })
+  //   this.result$ = this.http.get(BGG_URL,{params}).subscribe({
+  //     next: r => {this.results = r as Result[]},
+  //     error: err => {
+  //       this.notFound = true
+  //       console.error('ERROR: ', err)
+  //     },
+  //     complete() {
+  //       console.warn('......COMPLETE: ')
+  //     }
+  //   })
 
-  }
+  // }
 
   // // ======== Method 2: ========
   // // GET http://localhost:8080/api/bgg?name=NAME
@@ -71,5 +73,27 @@ export class AppComponent implements OnInit{
   //     .subscribe({})
 
   // }
+
+   // // ======== Method 3 (MongoDB): ========
+  // // GET http://localhost:8080/api/bgg/mongo?name=NAME
+  search(){
+
+    this.notFound = false // reset notFound
+
+    // create query param
+    const params = new HttpParams().set('name',this.form.value['name'])
+
+    this.result$ = this.http.get(BGG_MONGO_URL,{params}).subscribe({
+      next: r => {this.mongoResult = r as MongoResult, console.info(">> r is ", r)},
+      error: err => {
+        this.notFound = true
+        console.error('ERROR: ', err)
+      },
+      complete() {
+        console.warn('......COMPLETE: ')
+      }
+    })
+
+  }
   
 }
--- src/app/models.ts
+++ src/app/models.ts
@@ -12,3 +12,22 @@ export interface Result{
     rating: number
     cText: string
 }
+
+export interface MongoResult{
+
+    gid: number
+    name: string
+    year: number
+    ranking: number
+    usersRated: number
+    url: string
+    image: string
+    reviews: Review[]
+}
+
+export interface Review{
+    cId: number
+    user: string
+    rating: number
+    cText: string
+}
